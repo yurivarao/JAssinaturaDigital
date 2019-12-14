a2792dc65f5a408578b181d984197501f04c1500
yurivarao <yurivarao@hotmail.com>
2019-12-13 23:12:12 -0300

    first commit

---
+++ src/jassinaturadigital/Main.java
@@ -0,0 +1,7 @@
+package jassinaturadigital;
+
+public class Main {
+     public static void main(String[] args){
+         System.out.println("INFO: Biblioteca para Assinatura Digital Genérica de Documentos");
+     } 
+}
